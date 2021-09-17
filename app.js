80ec4f370b61389834d58fb7db2b58f9a78dce0d
vueno <uenovale@gmail.com>
2021-09-17 15:48:46 -0300

    entrega2

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
-// consulta nombre al usuario....
+// pedido de merchandising.....
 
-let nombre = prompt ("cual es tu nombre?");
-let bienvenida = "bienvenida" + " " + nombre;
-alert (bienvenida);
+const merch1 = "cuaderno"
+const merch2 = "llavero"
+const merch3 = "sticker"
+const merch4 = "medias"
 
-// consulta numero de socio....
+let merchandising = prompt ("que producto elige?")
 
-let numeroDeSocio = prompt ("ingrese numero de socio")
-let socio = "socio" + " " + numeroDeSocio;
+if (merchandising == merch1 || merchandising == merch2 || merchandising == merch3) {
+    console.log ('disponible');
+}  else {
+    console.log ('no esta disponible')
+}
 
-console.log (nombre + " " + socio);
+let merch1Cant = 2
+let merch2Cant = 1
+let merch3Cant = 3
+let merch4Cant = 0
 
-// consulta edad y calculo ....
-let edadSocio = prompt ("indique su edad");
+let merchChoose = Number (prompt ("cantidad?"))
 
-let añoActual = 2021;
-let calculoAñoNacimiento = añoActual - edadSocio;
-let nacimientoParrafo = "usted nacio en el año";
-console.log (nacimientoParrafo + " " + calculoAñoNacimiento);
+//eleccion cuadernos ....................
+if (merchChoose === merch1 || merchChoose ===1) {
+    console.log ("1 unidad en el carrito")
+} else if (merchChoose === merch1 || merchChoose ===2) {
+    console.log ("2 unidades en el carrito")
+} else {
+    console.log ("no hay suficiente")
+}
 
-// calculo egresado escuela .....
+//eleccion llavero..........
+if (merchChoose === merch2 || merchChoose ===1) {
+    console.log ("1 unidad en el carrito")
+} else {
+    console.log ("no hay suficiente")
+}
 
-let añoFinEscolar = Number (prompt ("indique año de egresado escuela"));
-let calculoAñoFinEscolar = añoActual - añoFinEscolar;
-console.log (nombre + " " + "finalizo la escuela hace" + " " + calculoAñoFinEscolar + " " + "años");
+//eleccion stickers.......
+if (merchChoose === merch3 || merchChoose ===1) {
+    console.log ("1 unidad en el carrito")
+} else if (merchChoose === merch3 || merchChoose ===2) {
+    console.log ("2 unidades en el carrito")
+} else if (merchChoose === merch3 || merchChoose ===3) {
+    console.log ("3 unidades en el carrito")
+} else {
+    console.log ("no hay suficiente")
+}
+
+
+
 
 
 
