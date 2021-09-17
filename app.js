59abfc70d743f6f084d5ddf5c436ec0f79a97ece
vueno <uenovale@gmail.com>
2021-09-17 15:56:43 -0300

    entregas 1 - 2

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+//entrega 1...................................................
+// consulta nombre al usuario....
+
+let nombre = prompt ("cual es tu nombre?");
+let bienvenida = "bienvenida" + " " + nombre;
+alert (bienvenida);
+
+// consulta numero de socio....
+
+let numeroDeSocio = prompt ("ingrese numero de socio")
+let socio = "socio" + " " + numeroDeSocio;
+
+console.log (nombre + " " + socio);
+
+// consulta edad y calculo ....
+let edadSocio = prompt ("indique su edad");
+
+let añoActual = 2021;
+let calculoAñoNacimiento = añoActual - edadSocio;
+let nacimientoParrafo = "usted nacio en el año";
+console.log (nacimientoParrafo + " " + calculoAñoNacimiento);
+
+// calculo egresado escuela .....
+
+let añoFinEscolar = Number (prompt ("indique año de egresado escuela"));
+let calculoAñoFinEscolar = añoActual - añoFinEscolar;
+console.log (nombre + " " + "finalizo la escuela hace" + " " + calculoAñoFinEscolar + " " + "años");
+
+
+//-------------------------------------------------------------------------------------------------------
+// entrega 2.........................................
+
 // pedido de merchandising.....
 
 const merch1 = "cuaderno"
