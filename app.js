32306f611a7f94524d3136375aa1e45023a2e143
vueno <uenovale@gmail.com>
2021-09-27 23:05:34 -0300

    agregar comentarios en desafio funciones 01

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 //declaro nombre y contraseñas correctas del usuario (ingresados en el registro)
 
-/*
 let usuario1 = "valeria"
 let contraseña1 = 2536
 
@@ -34,29 +33,32 @@ function ingreso () {
 
 ingreso ();
 
-*/
 //--------------------------------------------------------------------------------------------------
 
 // compra de producto: ////
 
+//declaro productos a vender en tienda de juegos.....
 let producto1 = "the last of us"
 let producto2 = "god of war"
 let producto3 = "ghost of tsushima"
 
+//declaro precios x unidad (neto)
 let netoPrecio1 = 500
 let netoPrecio2 = 600
 let netoPrecio3 = 800
 
+// creo funciones de suma, IVA y multiplicacion....
 const suma = (neto, iva) => neto + iva ;
 const iva = (x) => x *0.21
 const mult = (unidad, cantidad) => unidad * cantidad
 
-
+// pidio informacion al usuario 
 let productoSeleccion = prompt ("elija juego")
 let productoCantidad = Number (prompt ("cantidad?"))
 
 let precioFinal = 0
 
+// uso condicionales segun precio elegido x el usuario 
 if (productoSeleccion === producto1) {
     precioFinal = mult (suma (netoPrecio1, iva (netoPrecio1)), productoCantidad)
     alert (`carrito de compras: ${precioFinal}`)
@@ -73,19 +75,26 @@ if (productoSeleccion === producto1) {
     alert (`no esta disponible`)
 }
 
+// precio final para el usuario mostrado en consola.....
 console.log (precioFinal)
 
+// declaro cupon de descuento y consulto al ususario si tiene uno....
 let descuentoCupon = 2525
 let consultaDescuento = Number (prompt ("ingrese numero descuento"))
 
 let precioConDescuento = 0
 
+// funcion para descuento......
 const descuento = (y) => y *0.9
 
+// condicional en el caso que tenga o no cupon de descuento y muestro precio final segun indique.
 if (consultaDescuento === descuentoCupon) {
     precioConDescuento = descuento (precioFinal)
     alert (`precio con 10% descuento = ${precioConDescuento}`)
 } else {
     alert (`No corresponde descuento, precio final = ${precioFinal}`)
 }
+
+
+
 
